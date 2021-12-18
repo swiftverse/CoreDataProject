d8d633b75ca9011572cb45630f5cc2ec67a60e96
shivgirja <amitsrivastava115@yahoo.com>
2021-12-18 17:52:12 +0530

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Amit Shrivastava on 18/12/21.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
